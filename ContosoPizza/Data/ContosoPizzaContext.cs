cbeaa59ee307e19c3cd7a9a291e4d4fe74ad00fb
W10User <W10User@5CD04794WV>
2023-02-15 00:42:20 +0400

    Add project files.

---
+++ ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data
+{
+    public partial class ContosoPizzaContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ ContosoPizza/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime OrderFulfilled { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Connection String Here");
+        }
+
+    }
+}
